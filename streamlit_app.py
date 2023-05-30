d6ea3c86540dbe1ac5d90dd3278aed2b18da40ea
ihyunjung <109563978+ihyunjung@users.noreply.github.com>
2023-05-30 15:15:06 +0900

    Update streamlit_app.py
    edit header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title('My Parents New Healthy Diner')
+st.title('My Mom''s New Healthy Diner')
 
-st.header('Breakfast Menu')
+st.header('Breakfast Favorites')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
 st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
