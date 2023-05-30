e50c479d6a96554b02126e45e5328341224e7a57
ihyunjung <109563978+ihyunjung@users.noreply.github.com>
2023-05-30 15:55:25 +0900

    Update streamlit_app.py
    add try-except

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,16 +34,17 @@ st.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
+except URLError as e:
+  st.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# st.text(fruityvice_response.json()) # just writes the date to the screen
-
-# take the json version of the response and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-st.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
 st.stop()
